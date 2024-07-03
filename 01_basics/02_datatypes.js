e9d436da904db2b6ab271f9a344da31ae6aed1fe
Adarshnice <adarshpatwaa@gmail.com>
2024-07-03 09:20:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Adarsh")
+
+
+let name = "Adarsh" //string datatype
+let age = 18  //number datatype
+let isLoggedIn = false // boolean datatype
+let state;
+
+//primitive datatypes
+// number => 2 to power 53
+// bigint  used when number is > than int (number)
+// string => ""- preferred  ''- use this as well but not preferred
+// boolean => true/false
+// null => standalone value
+// undefined => where value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
