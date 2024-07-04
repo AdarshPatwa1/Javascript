f53cbd1aa0d838f39ffd3fc1f74fec31f2c3c791
Adarshnice <adarshpatwaa@gmail.com>
2024-07-04 06:24:53 +0000

    Variable basics

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,11 +1,11 @@
 const accountId = 1443   //const values is not changeable once declared
 //const is a constant
-let accountemail = "adarsh@gm"  //const and var are mostly usable
+let accountemail = "adarsh@gm"  //const and let are mostly usable
 //let is a variable 
 var accountpassword = "adarsh"  /*prefer not to use VAR
 because of issue in block scope and functional scope*/
 //var is a variable
 let accountcity = "bangalore"
-let accountstate
+let accountstate    //undefined
 console.table([accountId, accountemail, accountpassword, accountstate])  //for printing multiple values
 console.log(accountcity)  //to print single value
