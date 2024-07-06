4de1c6d23234420484ce11df7f467d1eea29a4cf
Adarshnice <adarshpatwaa@gmail.com>
2024-07-06 12:37:42 +0000

    math and num

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -19,12 +19,12 @@ const hundreds = 1000000
 // console.log(Math);  Object [Math] {}
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));  //it will round according to maths format
-// console.log(Math.ceil(4.2));   //it will round as highest 4.2 = 5
-// console.log(Math.floor(4.9));  //it will round as lowest 4.9 = 4
+// console.log(Math.ceil(4.2));   //it will round as max round off value 4.2 = 5
+// console.log(Math.floor(4.9));  //it will round as min round off value 4.9 = 4
 // console.log(Math.min(4, 3, 6, 8));  
 // console.log(Math.max(4, 3, 6, 8));
 
-//console.log(Math.random());
+//console.log(Math.random()); // value is between 0 and 1 only
 //console.log((Math.random()*10) + 1);
 //console.log(Math.floor(Math.random()*10) + 1);
 
