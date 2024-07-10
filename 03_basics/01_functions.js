60c89349f028ed108f5ff2e37ce416da810e9854
Adarshnice <adarshpatwaa@gmail.com>
2024-07-10 13:11:46 +0000

    parameters of function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,37 @@ function loginusermessage(username) {
 }
 
 // console.log(loginusermessage("adarsh"))
-console.log(loginusermessage())
+// console.log(loginusermessage())
+
+function calculatecartprice(val1, val2, ...num1){  // ... is spread operator
+     return num1  
+
+}
+
+console.log(calculatecartprice(200, 400, 500));  // befoe val1, val2 - [ 200, 400, 500 ]
+console.log(calculatecartprice(200, 400, 500, 2000));  // after val1, val2 - [ 500, 2000 ]
+
+const user = {
+   username: "adarsh",
+   price: 199
+}
+
+function handleobject(anyobject) {
+    console.log('username is ${anyobject.username} and price is ${anyobject.price}');
+
+}
+
+// handleobject(user)
+handleobject({
+    username: "sam",
+    price: 399
+})
+
+const mynewarray = [200, 400, 100, 600]
+
+function returnsecondvalue(getarray) {
+    return getarray[1]
+}
+
+// console.log(returnsecondvalue(mynewarray));  //400
+console.log(returnsecondvalue([200, 400, 500, 1000]));
